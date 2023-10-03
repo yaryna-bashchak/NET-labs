31be2f303539b48bbdc0a13d6747ec9025e9612f
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-10-03 20:53:11 +0300

    initialize nunit project for tests

---
+++ lab1/DequeLibrary.Tests/DequeTests.cs
@@ -0,0 +1,13 @@
+using DequeLibrary;
+
+namespace DequeLibrary.Tests;
+
+[TestFixture]
+public class DequeTests
+{
+    [Test]
+    public void PassedTest()
+    {
+        Assert.True(true);
+    }
+}
