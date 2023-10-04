d6a5f0b18469b041355af5ec75d8f6d7c59024c9
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-10-04 13:30:04 +0300

    write tests on TryRemoveFirst and TryRemoveLast

--- lab1/DequeLibrary.Tests/DequeTests.cs
+++ lab1/DequeLibrary.Tests/DequeTests.cs
@@ -108,7 +108,82 @@ public class DequeTests
     [Test]
     public void RemoveLast_EmptyDeque_ThrowExeption()
     {
-        var deque = new Deque<int>();
+        var deque = new Deque<string>();
+
+        Assert.Throws<InvalidOperationException>(() => deque.RemoveLast());
+    }
+
+    [Test]
+    public void TryRemoveFirst_NotEmptyDeque_ReturnsTrueAndFirstItem()
+    {
+        var deque = new Deque<string>(new string[] { "abc", "def", "ghi" });
+
+        var result = deque.TryRemoveFirst(out string? first);
+
+        Assert.That(deque.Count, Is.EqualTo(2));
+        Assert.That(first, Is.EqualTo("abc"));
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void TryRemoveFirst_OneItemInDeque_ReturnsTrueAndFirstItem()
+    {
+        var deque = new Deque<string>(new string[] { "abc" });
+
+        var result = deque.TryRemoveFirst(out string? first);
+
+        Assert.That(deque.Count, Is.EqualTo(0));
+        Assert.That(first, Is.EqualTo("abc"));
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void TryRemoveFirst_EmptyDeque_ReturnsFalseAndDefaultItem()
+    {
+        var deque = new Deque<string>();
+
+        var result = deque.TryRemoveFirst(out string? first);
+
+        Assert.That(deque.Count, Is.EqualTo(0));
+        Assert.That(first, Is.EqualTo(default));
+        Assert.That(result, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void TryRemoveLast_NotEmptyDeque_ReturnsTrueAndLastItem()
+    {
+        var deque = new Deque<string>(new string[] { "abc", "def", "ghi" });
+
+        var result = deque.TryRemoveLast(out string? last);
+
+        Assert.That(deque.Count, Is.EqualTo(2));
+        Assert.That(last, Is.EqualTo("ghi"));
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void TryRemoveLast_OneItemInDeque_ReturnsTrueAndLastItem()
+    {
+        var deque = new Deque<string>(new string[] { "abc" });
+
+        var result = deque.TryRemoveLast(out string? last);
+
+        Assert.That(deque.Count, Is.EqualTo(0));
+        Assert.That(last, Is.EqualTo("abc"));
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void TryRemoveLast_EmptyDeque_ReturnsFalseAndDefaultItem()
+    {
+        var deque = new Deque<string>();
+
+        var result = deque.TryRemoveFirst(out string? last);
+
+        Assert.That(deque.Count, Is.EqualTo(0));
+        Assert.That(last, Is.EqualTo(default));
+        Assert.That(result, Is.EqualTo(false));
+    }
 
         Assert.Throws<InvalidOperationException>(() => deque.RemoveLast());
     }
