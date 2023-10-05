0fb6e5effc483e4355ecd9a592fb69ac9d9e0fb2
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-10-05 15:55:36 +0300

    write tests on constructors

--- lab1/DequeLibrary.Tests/DequeTests.cs
+++ lab1/DequeLibrary.Tests/DequeTests.cs
@@ -6,6 +6,33 @@ namespace DequeLibrary.Tests;
 public class DequeTests
 {
     [Test]
+    public void DefaultConstructor_InitializesEmptyDeque()
+    {
+        var deque = new Deque<string>();
+
+        Assert.That(deque.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Constructor_GivenCollectionIsNull_ThrowArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Deque<string>(null));
+    }
+
+    [Test]
+    public void Constructor_WithCollection_InitializesDequeWithCorrectValues()
+    {
+        var collection = new List<string> { "abc", "def", "ghi" };
+
+        var deque = new Deque<string>(collection);
+
+        Assert.That(deque.Count, Is.EqualTo(3));
+        Assert.That(deque.Contains("abc"), Is.True);
+        Assert.That(deque.Contains("def"), Is.True);
+        Assert.That(deque.Contains("ghi"), Is.True);
+    }
+
+    [Test]
     public void AddFirst_EmptyDeque_AddsItemsToFront()
     {
         var deque = new Deque<string>();
