935ab8acb9457528a29e8b2785360f585898f960
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-10-05 14:59:24 +0300

    write tests on GetEnumerator

--- lab1/DequeLibrary.Tests/DequeTests.cs
+++ lab1/DequeLibrary.Tests/DequeTests.cs
@@ -372,4 +372,33 @@ public class DequeTests
 
         Assert.That(deque.Count, Is.EqualTo(0));
     }
+
+    [Test]
+    public void GetEnumerator_EmptyDeque_YieldsNoResult()
+    {
+        var deque = new Deque<string>();
+
+        var result = new List<string>();
+        foreach (var item in deque)
+        {
+            result.Add(item);
+        }
+
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void GetEnumerator_NotEmptyDeque_YieldsCorrectResult()
+    {
+        var deque = new Deque<string>(new string[] { "abc", "def", "ghi" });
+
+        var result = new List<string>();
+        foreach (var item in deque)
+        {
+            result.Add(item);
+        }
+
+        var expected = new List<string> { "abc", "def", "ghi" };
+        Assert.That(result, Is.EqualTo(expected));
+    }
 }
